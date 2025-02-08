dfab71ef1b67b7d179c883b5ccc986ef0515d696
Mihail Kirov <me@mikirov.xyz>
2025-02-08 17:46:55 +0200

    feat: add docs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import type { Plugin } from "@elizaos/core";
 import transferAction from "./actions/transfer.ts";
+import batchTransferAction from "./actions/batchTransfer.ts";
 import { WalletProvider, nativeWalletProvider } from "./providers/wallet.ts";
 
-export { WalletProvider, transferAction as TransferTonToken };
+export { WalletProvider, transferAction as TransferTonToken, batchTransferAction as BatchTransferTokens };
 
 export const tonPlugin: Plugin = {
     name: "ton",
     description: "Ton Plugin for Eliza",
-    actions: [transferAction],
+    actions: [transferAction, batchTransferAction],
     evaluators: [],
     providers: [nativeWalletProvider],
 };
