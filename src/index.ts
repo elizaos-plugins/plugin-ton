2ed25f465a89ef6578a91d52437c09a015de7f78
dijor0310 <dijor0310@gmail.com>
2025-02-13 20:28:00 +0100

    scaffold ton connect actions

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import type { Plugin } from "@elizaos/core";
 import transferAction from "./actions/transfer.ts";
 import { WalletProvider, nativeWalletProvider } from "./providers/wallet.ts";
+import tonConnectInitAction from "./actions/tonConnectInit.ts";
+import tonConnectTransferAction from "./actions/tonConnectTransfer.ts";
 
-export { WalletProvider, transferAction as TransferTonToken };
+export { WalletProvider,
+    transferAction as TransferTonToken,
+    tonConnectTransferAction as TonConnectTransferTonToken,
+    tonConnectInitAction as TonConnectInit,
+};
 
 export const tonPlugin: Plugin = {
     name: "ton",
     description: "Ton Plugin for Eliza",
-    actions: [transferAction],
+    actions: [transferAction, tonConnectTransferAction],
     evaluators: [],
     providers: [nativeWalletProvider],
 };
