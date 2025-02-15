c030051c3e1687e039f77e0d9c4a03c6b4496bda
Peter <kompaszbence@gmail.com>
2025-02-15 21:28:00 +0200

    chore: update types, add supportMethods method

--- src/providers/dexes/dex.d.ts
+++ src/providers/dexes/dex.d.ts
@@ -1,30 +1,39 @@
 import { JettonMaster } from "@ton/ton";
 
 export type Token = {
-    address: string;
-    name: string;
+  address: string;
+  name: string;
 };
 
+export enum SupportedMethod {
+  CREATE_POOL,
+  DEPOSIT,
+  WITHDRAW,
+  CLAIM_FEE,
+}
+
 export interface DEX {
-    createPool: (params: { isTon: boolean; jettons: JettonMaster[] }) => {};
-    // LP tokens should be issued
-    deposit: (params: {
-        jettonDeposits: JettonDeposit[];
-        isTon: boolean;
-        tonAmount: number;
-    }) => {};
-    // LP tokens should be burned
-    withdraw: (params: {
-        jettonWithdrawals: JettonWithdrawal[];
-        isTon: boolean;
-        amount: number;
-    }) => {};
-    claimFee: (params: { isTon: boolean; jettons: JettonMaster[] }) => {};
+  getSupportMethods: () => SupportedMethod[];
+  createPool?: (jettons: JettonMaster[]) => {};
+  // LP tokens should be issued
+  deposit?: (
+    jettonDeposits: JettonDeposit[],
+    tonAmount: number,
+    params: {}
+  ) => {};
+  // LP tokens should be burned
+  withdraw?: (
+    jettonWithdrawals: JettonWithdrawal[],
+    isTon: boolean,
+    amount: number,
+    params: {}
+  ) => {};
+  claimFee?: (params: { isTon: boolean; jettons: JettonMaster[] }) => {};
 }
 
 export type JettonDeposit = {
-    jetton: JettonMaster;
-    amount: number;
+  jetton: JettonMaster;
+  amount: number;
 };
 
 export type JettonWithdrawal = JettonDeposit;
