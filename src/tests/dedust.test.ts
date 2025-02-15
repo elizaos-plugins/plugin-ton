0b128c844043ba24caf69345894d16838dd46f38
Peter <kompaszbence@gmail.com>
2025-02-15 21:28:35 +0200

    feat: dedust tests

---
+++ src/tests/dedust.test.ts
@@ -0,0 +1,70 @@
+import { defaultCharacter } from "@elizaos/core";
+
+import { describe, it, vi, expect, beforeAll } from "vitest";
+import { Dedust } from "../providers/dexes/dedust";
+
+import { mnemonicNew, mnemonicToPrivateKey, type KeyPair } from "@ton/crypto";
+import { Address, JettonMaster } from "@ton/ton";
+
+const SCALE_ADDR = "EQBlqsm144Dq6SjbPI4jjZvA1hqTIP3CvHovbIfW_t-SCALE";
+
+describe("DEX: DeDust", () => {
+  let keypair: KeyPair;
+  let mockedRuntime;
+  let dedustClient;
+
+  beforeAll(async () => {
+    const password = "";
+    const mnemonics: string[] = await mnemonicNew(12, password);
+    keypair = await mnemonicToPrivateKey(mnemonics, password);
+    mockedRuntime = {
+      character: defaultCharacter,
+    };
+    dedustClient = new Dedust();
+    await dedustClient.init(mockedRuntime);
+  });
+
+  describe("Create Pool", () => {
+    it("should get vault", async () => {});
+
+    it("should get pool", async () => {
+      const scale = new JettonMaster(Address.parse(SCALE_ADDR));
+      const usdt = new JettonMaster(
+        Address.parse("EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs")
+      );
+      const aqua = new JettonMaster(
+        Address.parse("EQAWDyxARSl3ol2G1RMLMwepr3v6Ter5ls3jiAlheKshgg0K")
+      );
+      const not = new JettonMaster(
+        Address.parse("EQAvlWFDxGF2lXm67y4yzC17wYKD9A0guwPkMs1gOsM__NOT")
+      );
+      let result = await dedustClient.getPool([scale]);
+      expect(result.address.toString()).toEqual(
+        "EQDcm06RlreuMurm-yik9WbL6kI617B77OrSRF_ZjoCYFuny"
+      );
+      result = await dedustClient.getPool([scale, not]);
+      expect(result.address.toString()).toEqual(
+        "EQDJN4L57W_05RrcUhnShICi0j02aCkFnhQugK0zQf0LL5WC"
+      );
+      result = await dedustClient.getPool([aqua, usdt]);
+      expect(result.address.toString()).toEqual(
+        "EQCm2vzJvYS2vtfp-jsPZTp-8AIRWMRHHYA2YmiQ54Hixp7W"
+      );
+    });
+
+    it("should create pool", async () => {
+      const build = new JettonMaster(
+        Address.parse("EQBYnUrIlwBrWqp_rl-VxeSBvTR2VmTfC4ManQ657n_BUILD")
+      );
+      const pavul = new JettonMaster(
+        Address.parse("EQChXgdfER2nuLvZP5EiSlCf95oXZ5KDiCEmGBBv_WcNdxz3")
+      );
+      const result = await dedustClient.createPool([build, pavul]);
+      expect(result).toEqual(true);
+    });
+  });
+
+  //   describe("Add liquidity", () => {});
+  //   describe("Withdraw liquidity", () => {});
+  //   describe("Claim Fee", () => {});
+});
