c388736ce2d9a377bf71f8c69e3fd8f5e0871c4e
Mihail Kirov <me@mikirov.xyz>
2025-02-21 15:23:08 +0200

    fixing merge conflicts

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import type { Plugin } from "@elizaos/core";
 import transferAction from "./actions/transfer.ts";
 import createWalletAction from "./actions/createWallet.ts";
 import loadWalletAction from "./actions/loadWallet.ts";
+import batchTransferAction from "./actions/batchTransfer.ts";
 import { WalletProvider, nativeWalletProvider } from "./providers/wallet.ts";
 
-export { WalletProvider, transferAction as TransferTonToken, createWalletAction as CreateTonWallet, loadWalletAction as LoadTonWallet };
+export { WalletProvider, transferAction as TransferTonToken, createWalletAction as CreateTonWallet, loadWalletAction as LoadTonWallet, batchTransferAction as BatchTransferTokens };
 
 export const tonPlugin: Plugin = {
     name: "ton",
     description: "Ton Plugin for Eliza",
     actions: [
         transferAction,
+        batchTransferAction,
         createWalletAction,
         loadWalletAction,
     ],
