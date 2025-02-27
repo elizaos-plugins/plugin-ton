874cee057d9cb151697c1b8667d649e15ba7c4f4
Peter <kompaszbence@gmail.com>
2025-02-27 20:11:08 +0200

    chore: add provider to plugin

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import type { Plugin } from "@elizaos/core";
 import transferAction from "./actions/transfer.ts";
 import { WalletProvider, nativeWalletProvider } from "./providers/wallet.ts";
+import { DexProvider, dexProvider } from "./providers/dex.ts";
+import dexAction from "./actions/dex.ts";
 
 export { WalletProvider, transferAction as TransferTonToken };
+export { DexProvider, dexAction };
 
-export const tonPlugin: Plugin = {
+export const tonPlugin2: Plugin = {
     name: "ton",
     description: "Ton Plugin for Eliza",
-    actions: [transferAction],
+    actions: [transferAction, ],
     evaluators: [],
-    providers: [nativeWalletProvider],
+    providers: [nativeWalletProvider, dexProvider],
 };
 
-export default tonPlugin;
+export default tonPlugin2;
